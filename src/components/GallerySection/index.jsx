ceeb95a10b2ffcf16c6ac6a558ac4d465308e5a1
Vasilii Perevozchikov <Vasilii.Perevozchikov@noveogroup.com>
2022-03-11 04:16:33 +0700

    Extend planes page

--- src/components/GallerySection/index.jsx
+++ src/components/GallerySection/index.jsx
@@ -3,7 +3,7 @@ import './style.css';
 import {useTranslation} from "react-i18next";
 
 export const Gallery = (props) => {
-  const [t, i18n] = useTranslation('common');
+  const [t] = useTranslation('common');
 
   return (
     <div id='portfolio' className='text-center'>
@@ -19,7 +19,7 @@ export const Gallery = (props) => {
             {props.data
               ? props.data.map((d, i) => (
                 <div key={`${d.title}-${i}`} className='col-sm-6 col-md-4 col-lg-4'>
-                  <Image title={d.title} largeImage={d.largeImage} smallImage={d.smallImage} />
+                  <Image title={t(`planes.${d.title}`)} largeImage={d.largeImage} smallImage={d.smallImage} />
                 </div>
               ))
               : 'Loading...'}
